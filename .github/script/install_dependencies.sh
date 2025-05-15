500368e3a68ceaaa6ad8fae0b5216d069f15ba79
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-05-15 10:11:51 +0200

    Add ci pipelines
    Also add configuration files for formatting with pre-commit.

---
+++ .github/script/install_dependencies.sh
@@ -0,0 +1,19 @@
+#/usr/bin/bash
+cd $HOME
+apt-get install -y socat
+
+# Python dependencies
+python_deps="requests"
+os_name=$(lsb_release -cs)
+
+case $os_name in
+  jammy) # Ubuntu 22.04
+    pip install --user $python_deps
+    ;;
+  kinetic) # Ubuntu 24.04
+    pip install --break-system-packages $python_deps
+    ;;
+  *) # Newer
+    pip install --break-system-packages $python_deps
+    ;;
+esac
