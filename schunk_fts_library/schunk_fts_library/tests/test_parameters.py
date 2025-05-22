97e644aa0e14c0b0fa1df98a37e0ed293a715c12
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-05-22 14:55:30 +0200

    Adapt parameter tests
    Use parameters that we can actually read and write.
    We also had a broken sensor before.
    It now works as expected.

--- schunk_fts_library/schunk_fts_library/tests/test_parameters.py
+++ schunk_fts_library/schunk_fts_library/tests/test_parameters.py
@@ -5,10 +5,9 @@ def test_driver_offers_getting_parameters():
     driver = Driver()
     driver.connect(host="192.168.0.100", port=82)
 
-    param = "1031"
-    expected_value = "01"
+    param = "0001"  # product_name
+    expected_value = "465453"  # FTS
     response = driver.get_parameter(index=param)
-    print(f"response: {response}")
     assert response.error_code == "00"
     assert response.param_value == expected_value
 
@@ -17,8 +16,7 @@ def test_driver_offers_setting_parameters():
     driver = Driver()
     driver.connect(host="192.168.0.100", port=82)
 
-    param = "1031"  # needs root access
-    expected_value = "01"
+    param = "0050"  # switch ft-signal units
+    expected_value = "00"
     response = driver.set_parameter(value=expected_value, index=param)
     assert response.error_code == "10"  # user level not sufficient
-    # assert response.param_value == expected_value
