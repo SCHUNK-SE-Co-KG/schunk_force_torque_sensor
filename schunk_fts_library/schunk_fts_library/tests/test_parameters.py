f3fa194859e9ab42bc96bc4709fba14fc164c9d4
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-06-03 15:47:57 +0200

    Use the new connection approach for parameter handling

--- schunk_fts_library/schunk_fts_library/tests/test_parameters.py
+++ schunk_fts_library/schunk_fts_library/tests/test_parameters.py
@@ -3,10 +3,9 @@ from schunk_fts_library.driver import Driver
 
 def test_driver_offers_getting_parameters():
     driver = Driver()
-    driver.connect(host="192.168.0.100", port=82)
 
     param = "0001"  # product_name
-    expected_value = "465453"  # FTS
+    expected_value = "465453"  # 465453: FTS, 4b4d53: KMS
     response = driver.get_parameter(index=param)
     assert response.error_code == "00"
     assert response.param_value == expected_value
@@ -14,7 +13,6 @@ def test_driver_offers_getting_parameters():
 
 def test_driver_offers_setting_parameters():
     driver = Driver()
-    driver.connect(host="192.168.0.100", port=82)
 
     param = "0050"  # switch ft-signal units
     expected_value = "00"
