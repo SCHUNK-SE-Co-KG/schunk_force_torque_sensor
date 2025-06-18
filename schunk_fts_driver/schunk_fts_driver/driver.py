e52e749d85ebd58838998636a675082d6a083e6d
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-06-18 17:21:02 +0200

    Add a minimalist ROS2 lifecycle node
    Also add basic test infrastructure.

---
+++ schunk_fts_driver/schunk_fts_driver/driver.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# Copyright 2025 SCHUNK SE & Co. KG
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------
+
+import rclpy
+from rclpy.lifecycle import Node, State, TransitionCallbackReturn
+from rclpy.executors import MultiThreadedExecutor, ExternalShutdownException
+from rcl_interfaces.msg import SetParametersResult
+
+
+class Driver(Node):
+
+    def __init__(self, node_name: str, **kwargs):
+        super().__init__(node_name, **kwargs)
+
+    def on_configure(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().debug("on_configure() is called.")
+        return TransitionCallbackReturn.SUCCESS
+
+    def on_activate(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().debug("on_activate() is called.")
+        return super().on_activate(state)
+
+    def on_deactivate(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().debug("on_deactivate() is called.")
+        return super().on_deactivate(state)
+
+    def on_cleanup(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().debug("on_cleanup() is called.")
+        return TransitionCallbackReturn.SUCCESS
+
+    def on_shutdown(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().debug("on_shutdown() is called.")
+        return SetParametersResult(successful=True)
+
+
+def main():
+    rclpy.init()
+    executor = MultiThreadedExecutor()
+    driver = Driver("driver")
+    executor.add_node(driver)
+    try:
+        executor.spin()
+    except (KeyboardInterrupt, ExternalShutdownException):
+        driver.destroy_node()
+
+
+if __name__ == "__main__":
+    main()
---
+++ schunk_fts_driver/schunk_fts_driver/tests/test_lifecycle_behavior.py
@@ -0,0 +1,59 @@
+# Copyright 2025 SCHUNK SE & Co. KG
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------------
+from lifecycle_msgs.msg import Transition, State
+
+
+def test_driver_supports_repeated_configure_and_cleanup(sensor, lifecycle_interface):
+    driver = lifecycle_interface
+    for _ in range(3):
+        driver.change_state(Transition.TRANSITION_CONFIGURE)
+        assert driver.check_state(State.PRIMARY_STATE_INACTIVE)
+        driver.change_state(Transition.TRANSITION_CLEANUP)
+        assert driver.check_state(State.PRIMARY_STATE_UNCONFIGURED)
+
+
+def test_driver_supports_repeated_activate_and_deactivate(sensor, lifecycle_interface):
+    driver = lifecycle_interface
+    driver.change_state(Transition.TRANSITION_CONFIGURE)
+    for _ in range(3):
+        driver.change_state(Transition.TRANSITION_ACTIVATE)
+        assert driver.check_state(State.PRIMARY_STATE_ACTIVE)
+        driver.change_state(Transition.TRANSITION_DEACTIVATE)
+        assert driver.check_state(State.PRIMARY_STATE_INACTIVE)
+    driver.change_state(Transition.TRANSITION_CLEANUP)
+
+
+def test_primary_lifecycle_states(sensor, lifecycle_interface):
+    driver = lifecycle_interface
+
+    # Startup
+    assert driver.check_state(State.PRIMARY_STATE_UNCONFIGURED)
+
+    # configure
+    driver.change_state(Transition.TRANSITION_CONFIGURE)
+    assert driver.check_state(State.PRIMARY_STATE_INACTIVE)
+
+    # activate
+    driver.change_state(Transition.TRANSITION_ACTIVATE)
+    assert driver.check_state(State.PRIMARY_STATE_ACTIVE)
+
+    # deactivate
+    driver.change_state(Transition.TRANSITION_DEACTIVATE)
+    assert driver.check_state(State.PRIMARY_STATE_INACTIVE)
+
+    # cleanup
+    driver.change_state(Transition.TRANSITION_CLEANUP)
+    assert driver.check_state(State.PRIMARY_STATE_UNCONFIGURED)
