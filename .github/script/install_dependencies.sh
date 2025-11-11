ff2573b7bd4965415c4629e1d1968c1814799734
ReinwaldFabian <fabian.reinwald@de.schunk.com>
2025-11-11 14:05:44 +0000

    Github action deps use same deps as packages

--- .github/script/install_dependencies.sh
+++ .github/script/install_dependencies.sh
@@ -2,18 +2,20 @@
 cd $HOME
 apt-get install -y socat
 
-# Python dependencies
-python_deps="requests"
+# Python dependencies from setup.py
 os_name=$(lsb_release -cs)
 
 case $os_name in
   jammy) # Ubuntu 22.04
-    pip install --user $python_deps
+    pip install --user -e /workspace/src/schunk_fts_library
+    pip install --user -e /workspace/src/schunk_fts_driver
     ;;
   kinetic) # Ubuntu 24.04
-    pip install --break-system-packages $python_deps
+    pip install --break-system-packages -e /workspace/src/schunk_fts_library
+    pip install --break-system-packages -e /workspace/src/schunk_fts_driver
     ;;
   *) # Newer
-    pip install --break-system-packages $python_deps
+    pip install --break-system-packages -e /workspace/src/schunk_fts_library
+    pip install --break-system-packages -e /workspace/src/schunk_fts_driver
     ;;
 esac
